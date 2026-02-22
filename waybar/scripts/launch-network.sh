5482cd8d061c9f7ec5b89f41c527e26d9b9b2d76
jihan431 <EMAIL_GITHUB_KAMU>
2026-02-22 21:06:52 +0700

    ADD CONTROL CENTER AND FIXING LAUNCHER

--- waybar/scripts/launch-network.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# launch-network.sh
-# Wrapper to launch networkmanager_dmenu with the custom theme
-
-# Launch custom rofi wifi menu
-/home/lyon/.config/waybar/scripts/rofi-wifi-menu.sh
